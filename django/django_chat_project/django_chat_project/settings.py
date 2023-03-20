ca68f214875ebd52d3e1c9946bd85ac16f460fe2
Tu Nombre <you@example.com>
2023-03-20 12:44:02 -0300

    Close #3 configuracion channel agregado

--- django/django_chat_project/django_chat_project/settings.py
+++ django/django_chat_project/django_chat_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_h@i!rh5%7-k0q%$cbzz7m8*plpykovh4i_#1z1_+xc(xqz_x*'
+# SECRET_KEY = 'django-insecure-_h@i!rh5%7-k0q%$cbzz7m8*plpykovh4i_#1z1_+xc(xqz_x*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
-    # 'channels',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+ASGI_APPLICATION = 'django_chat_project.asgi.application'
 
 WSGI_APPLICATION = 'django_chat_project.wsgi.application'
 
@@ -82,7 +83,14 @@ DATABASES = {
     }
 }
 
-
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
