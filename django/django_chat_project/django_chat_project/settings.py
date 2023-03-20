372f6e8d1ea92fb0229c6d3afab7174a8f9c1d15
j0k3r <agm.moya@alumno.um.edu.ar>
2023-03-20 13:51:07 -0300

    agregar secret_key a .env

--- django/django_chat_project/django_chat_project/settings.py
+++ django/django_chat_project/django_chat_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-_h@i!rh5%7-k0q%$cbzz7m8*plpykovh4i_#1z1_+xc(xqz_x*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
-    'channels',
+    # 'channels',
 ]
 
 MIDDLEWARE = [
