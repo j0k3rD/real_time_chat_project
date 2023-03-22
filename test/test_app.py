ece4f9db17ffcda973deac3b856f41391237e847
Aaron Moya <83615514+j0k3rD@users.noreply.github.com>
2023-03-22 11:03:05 -0300

    Delete test directory

--- test/test_app.py
+++
@@ -1,8 +0,0 @@
-#Test unitarios para la aplicación Django
-
-from django.test import TestCase
-
-
-class TestApp(TestCase):
-    def test_app(self):
-        self.assertEqual("real_time_chat_project", "real_time_chat_project")
