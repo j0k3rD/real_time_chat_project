9db86f042290b31ad7a64a8447a812b3a3e5d9e4
j0k3r <agm.moya@alumno.um.edu.ar>
2023-03-27 13:12:46 -0300

    #12 Tests unitarios andando

--- user_service/user/models.py
+++ user_service/user/models.py
@@ -5,4 +5,4 @@ class User(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     username = models.CharField(max_length=20, null=False, unique=True)
     password = models.CharField(max_length=20, null=False)
-    email = models.EmailField(max_lenght=50, null=False, unique=True)
+    email = models.EmailField(max_length=50, null=False, unique=True)
