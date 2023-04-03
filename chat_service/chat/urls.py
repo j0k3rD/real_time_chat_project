461720fd0fc80d110b912e0fa0e12c227fe326c2
Tu Nombre <you@example.com>
2023-04-03 15:13:04 -0300

    Agregadas estructuras en html y urls

--- chat_service/chat/urls.py
+++ chat_service/chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('group/<int:group_id>/', views.group_chat, name='group_chat'), 
+    path('', views.chat_main_page, name='chat_main_page'),
     # path('send_message/', views.send_message, name='send_message'),
 ]
