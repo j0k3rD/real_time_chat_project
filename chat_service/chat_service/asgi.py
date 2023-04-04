3e78080c856ba373db9732301ff3954a73404fee
Nicolas <n.mayoral@alumno.um.edu.ar>
2023-04-04 18:58:26 -0300

    #19 Docker run andando, gunicorn no funciona

--- chat_service/chat_service/asgi.py
+++ chat_service/chat_service/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_service.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- chat_service/chat_service/wsgi.py
+++ chat_service/chat_service/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_service.settings')
 
 application = get_wsgi_application()
