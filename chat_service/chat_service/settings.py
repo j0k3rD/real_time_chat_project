44c979f0de874465881b7a40de0a01a7a58ac7bd
j0k3r <agm.moya@alumno.um.edu.ar>
2023-04-04 13:22:15 -0300

    #15 Agrego la configuracion de Gunicorn y Uvicorn para servidor asgi, agrego las dependencias a req_prod

--- chat_service/chat_service/settings.py
+++ chat_service/chat_service/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.18.18']
 
 
 # Application definition
