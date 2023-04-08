69e228d5b79bb5a48e9dcc74ed90fc3cfacf9b9f
Aaron <agm.moya@alumno.um.edu.ar>
2023-04-08 12:31:04 -0300

    cambio la url de layers al del contenedor redis

--- chat_service/chat_service/settings.py
+++ chat_service/chat_service/settings.py
@@ -106,7 +106,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('127.0.0.1', 6378)],
+            "hosts": [('172.18.0.1', 6378)],
         },
     },
 }
