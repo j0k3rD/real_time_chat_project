ad1d185b4b56452a2608a9dd9327f096691586e9
Markitos <48894558+XxRaXoRxX@users.noreply.github.com>
2023-04-10 17:31:14 -0300

    #20 Microservicio Usuario funcionando

--- user_service/user/models.py
+++ user_service/user/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-
-# Se utiliza el User de Django por defecto. Por eso lo comento.
 # User model with id, username, password, email.
-#class User(models.Model):
-#    id = models.AutoField(primary_key=True, unique=True)
-#    username = models.CharField(max_length=20, null=False, unique=True)
-#    password = models.CharField(max_length=20, null=False)
-#    email = models.EmailField(max_length=50, null=False, unique=True)
+class User(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    username = models.CharField(max_length=20, null=False, unique=True)
+    password = models.CharField(max_length=20, null=False)
+    email = models.EmailField(max_length=50, null=False, unique=True)
