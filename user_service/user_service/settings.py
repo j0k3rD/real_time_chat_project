607ce34c1048d489438089c2d717641897da505e
Markitos <48894558+XxRaXoRxX@users.noreply.github.com>
2023-04-11 17:34:43 -0300

    #20 Agregado vistas de registro en microservicio de user

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
 
 # Application definition
