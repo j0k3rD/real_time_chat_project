6eb7a11d444a6d709732e04f38fd97c85a3576d3
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-01 19:57:19 -0300

    #18 username websocket configurado, estilo img menu implementado

--- install_userservice.sh
+++ install_userservice.sh
@@ -1,3 +1,3 @@
 cd user_service/
 # Creamos la imagen de Docker. Debemos editar la version cada vez que implementemos algo nuevo en el servicio
-docker build -t userservice:v0.4.0 .
+docker build -t userservice:v0.6.0 .
