dbc32fc3ff5154597461c9f54d4ccb58c1be04b4
Markitos <48894558+XxRaXoRxX@users.noreply.github.com>
2023-05-01 22:36:03 -0300

    #26 Merge

--- user_service/user/functions.py
+++ user_service/user/functions.py
@@ -15,11 +15,33 @@ def get_tokens_for_user(user):
     }
     
 def remove_tokens(refresh_token):
+    try:
+        refresh = RefreshToken(refresh_token)
+        refresh.blacklist()
+        return True
+    except:
+        return False
+    
+def refresh_token(refresh_token):
+    try:
+        refresh = RefreshToken(refresh_token)
+        accessToken = refresh.access_token
 
-        encoded = jwt.encode(decodeJTW, config('SECRET_KEY'), algorithm="HS256")
+        userService = UserService()
+        user = userService.get_by_id(refresh['user_id'])
 
+        encoded = insert_claims(accessToken, user.get_username(), user.get_email())
         return {
             'refresh': str(refresh),
-            'access': str(encoded),
-        }
-            return encoded
+            'access_token': str(encoded),
+            }
+    except:
+        return None
+    
+def insert_claims(accessToken, username, email):
+    decodeJTW = jwt.decode(str(accessToken), config('SECRET_KEY'), algorithms=["HS256"])
+    # add payload here!!
+    decodeJTW['username'] = username
+    decodeJTW['email'] = email
+    encoded = jwt.encode(decodeJTW, config('SECRET_KEY'), algorithm="HS256")
+    return encoded
