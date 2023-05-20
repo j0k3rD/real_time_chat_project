7fc5d70409a46de4ce5b0971578993a33f362624
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-20 13:51:44 -0300

    #35 React project configuracion en userservice Started

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'user_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'user/templates/user'],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -164,6 +164,9 @@ USE_TZ = True
 
 STATIC_URL = config('STATIC_PATH')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- user_service/user_service/urls.py
+++ user_service/user_service/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
