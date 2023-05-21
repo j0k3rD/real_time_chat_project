52edcde658d9226194abe47df620cc95c9d58869
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-21 12:00:47 -0300

    #35 cambio staticfiles_dirs

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 STATIC_URL = config('STATIC_PATH')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
 # Default primary key field type
