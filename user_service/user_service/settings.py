717de1b5cb7100edbce972b95e3624a162d3e3ba
Aaron <agm.moya@alumno.um.edu.ar>
2023-08-09 10:06:20 -0300

    #44 In UserService: Updating versions in req, fixing settings, r0.8.0

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -211,3 +211,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.localhost']
